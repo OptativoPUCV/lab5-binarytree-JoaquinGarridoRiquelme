5d2c211beebe1a5e09d4554dec94e7e605d71c5b
Your Name <you@example.com>
2024-05-09 02:39:43 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -81,7 +81,7 @@ void insertTreeMap(TreeMap * tree, void* key, void * value)
                 aux = aux -> left;
         }
             
-        else if(tree-> lower_than(aux -> pair -> key, key))
+        else 
         {
             if(aux -> right == NULL)
             {
