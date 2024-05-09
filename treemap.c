d7da9d3bfb65438c2b2f2f7a2c9cc2e96314ff49
Your Name <you@example.com>
2024-05-09 03:37:46 +0000

    new try

--- treemap.c
+++ treemap.c
@@ -98,14 +98,31 @@ TreeNode * minimum(TreeNode * x)
 void removeNode(TreeMap * tree, TreeNode* node) 
 {
     if(tree->root == node) tree->root = NULL;
-
+    TreeNode *hijo;
+    
     //Nodo sin hijos
     if(node->left == NULL && node ->right == NULL) node = NULL;
 
     //Nodo con un hijo
     else if(node->left == NULL || node ->right == NULL)
     {
-        //if(node->left == NULL)
+        if(node->right != NULL)
+            hijo = node -> right;
+            
+        else if(node ->left != NULL)
+            hijo = node -> left;
+
+        if(node -> parent == NULL)
+            tree->root = hijo;
+
+        else if(node -> parent -> left == node)
+            node -> parent -> left = hijo;
+
+        else
+            node -> parent -> right = hijo;
+
+        if(hijo != NULL)
+            hijo -> parent = node -> parent;
     }
 }
 
